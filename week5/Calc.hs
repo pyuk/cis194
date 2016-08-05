1b41157588a5608726511672da886bbe8861be6a
pyuk <psi.razputin@gmail.com>
2016-08-04 22:23:36 -0500

    more week 5

--- week5/Calc.hs
+++ week5/Calc.hs
@@ -25,6 +25,9 @@ eval' (Just (Mul x y)) = eval' (Just x) * eval' (Just y)
 evalStr :: String -> Maybe Integer
 evalStr xs = eval' $ parseExp Lit Add Mul xs
 
+evalStr' :: String -> Maybe Integer
+evalStr' = fmap eval . parseExp Lit Add Mul
+
 --exercise 3
 
 class Expr a where
@@ -36,3 +39,45 @@ instance Expr ExprT where
    lit x = Lit x
    add x y = Add x y
    mul x y = Mul x y
+
+--exercise 4
+
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+newtype Mod7   = Mod7 Integer deriving (Eq, Show)
+
+instance Expr Integer where
+    lit x = x
+    add x y = x + y
+    mul x y = x * y
+
+instance Expr Bool where
+    lit x = if x <= 0 then False else True
+    add x y = x || y
+    mul x y = x && y
+
+instance Ord MinMax where
+    max (MinMax x) (MinMax y) = MinMax $ max x y
+    min (MinMax x) (MinMax y) = MinMax $ min x y
+    
+instance Expr MinMax where
+    lit x = MinMax x
+    add x y = max x y
+    mul x y = min x y
+
+instance Expr Mod7 where
+    lit x = Mod7 $ x `mod` 7
+    add (Mod7 x) (Mod7 y) = Mod7 $ (x + y) `mod` 7
+    mul (Mod7 x) (Mod7 y) = Mod7 $ (x * y) `mod` 7
+
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
+
+testInteger = testExp :: Maybe Integer
+testBool    = testExp :: Maybe Bool
+testMM      = testExp :: Maybe MinMax
+testSat     = testExp :: Maybe Mod7
+
+--exercise 5
+
+instance Expr Program where
+   lit x = [PushI x]
