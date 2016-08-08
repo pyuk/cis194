19b2f679f907533227e031081217def4c21b8c20
pyuk <psi.razputin@gmail.com>
2016-08-07 22:54:24 -0500

    finished week 6

--- week6/Fibonacci.hs
+++ week6/Fibonacci.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# OPTIONS -Wall #-}
 module Fibonacci where
 
 --exercise 1
@@ -37,7 +39,7 @@ streamMap :: (a -> b) -> Stream a -> Stream b
 streamMap f (Cons n ns) = Cons (f n) (streamMap f ns)
 
 streamFromSeed :: (a -> a) -> a -> Stream a
-streamFromSeed f n = Cons n (streamFromSeed f (f n))
+streamFromSeed f n = Cons n $ streamFromSeed f (f n)
 
 --exercise 5
 
@@ -45,8 +47,43 @@ nats :: Stream Integer
 nats = streamFromSeed (+1) 0
 
 interleaveStreams :: Stream a -> Stream a -> Stream a
-interleaveStreams (Cons n ns) (Cons l ls) = Cons n (Cons l (interleaveStreams ns ls))
+interleaveStreams (Cons n ns) ls = Cons n $ interleaveStreams ls ns
 
 ruler :: Stream Integer
-ruler = interleaveStreams (streamRepeat 0) (ruler' 1)
-    where ruler' n = interleaveStreams (streamRepeat n) (ruler' $ n + 1)
+ruler = interleaveStreams (streamRepeat 0) (f 1)
+    where f n = interleaveStreams (streamRepeat n) (f $ n + 1)
+
+--exercise 6
+
+x :: Stream Integer
+x = Cons 0 (Cons 1 (streamRepeat 0))
+
+instance Num (Stream Integer) where
+    fromInteger n = Cons n (streamRepeat 0)
+    negate (Cons n ns) = Cons (-n) (negate ns)
+    (+) (Cons n ns) (Cons l ls) = Cons (n + l) (ns + ls)
+    (*) (Cons n ns) cs@(Cons l ls) = Cons (n * l) $ (streamMap (*n) ls) + (ns * cs)
+
+instance Fractional (Stream Integer) where
+    (/) (Cons n ns) (Cons l ls) = q
+        where q = Cons (n `div` l) $ streamMap (`div` l) (ns - (q * ls))
+
+fibs3 :: Stream Integer
+fibs3 = x / (1 - x - x^2)
+
+--exercise 7
+
+data Matrix = Matrix (Integer, Integer, Integer, Integer)
+
+instance Num Matrix where
+    (*) (Matrix (a1,a2,a3,a4)) (Matrix (x1,x2,x3,x4)) = 
+        Matrix (a1*x1+a2*x3, a1*x2+a2*x4, a3*x1+a4*x3, a3*x2+a4*x4)
+
+fib4 :: Integer -> Integer
+fib4 n
+    | n == 0 = 0
+    | n == 1 = 1
+    | even n = b $ (f ^ (n `div` 2)) ^ 2
+    | otherwise = b $ f * (f ^ ((n-1) `div` 2))^2
+    where f = Matrix (1,1,1,0)
+          b (Matrix (_,x,_,_)) = x
