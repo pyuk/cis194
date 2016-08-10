96caf12a8e12b01559bfee6b011cd4b76481a262
pyuk <psi.razputin@gmail.com>
2016-08-09 21:15:44 -0500

    week 7 work

--- week7/JoinList.hs
+++ week7/JoinList.hs
@@ -1,20 +1,58 @@
 module JoinList where
 
 import Data.Monoid
+import Sized
 
 data JoinList m a = Empty
                   | Single m a
                   | Append m (JoinList m a) (JoinList m a)
-    deriving (Eq, Show)
+  deriving (Eq, Show)
+
+--exercise 1
 
 tag :: Monoid m => JoinList m a -> m
-tag Empty          = Empty
+tag Empty          = mempty
 tag (Append m _ _) = m
 tag (Single m _)   = m
 
 (+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
-(+++) Empty Empty = Empty
-(+++) Empty l = l
-(+++) r Empty = r
-(+++) (Single m x) (Single m2 y) = Single (m <> m2) (x <> y)
-(+++) (Append m r) (Append m2 l) = Append (m <> m2) (r +++ l)
+--(+++) Empty Empty = Empty
+--(+++) Empty r = r
+--(+++) l Empty = l
+(+++) l r = Append (tag l <> tag r) l r
+
+--exercise 2
+
+(!!?) :: [a] -> Int -> Maybe a
+[]     !!? _ = Nothing
+_      !!? i | i < 0 = Nothing
+(x:xs) !!?  0 = Just x
+(x:xs) !!? i = xs !!? (i-1)
+
+jlToList :: JoinList m a -> [a]
+jlToList Empty = []
+jlToList (Single _ a) = [a]
+jlToList (Append _ l1 l2) = jlToList l1 ++ jlToList l2
+
+indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
+indexJ _ Empty = Nothing
+indexJ n _ | n < 0 = Nothing
+indexJ n (Single a b)
+    | n == 0    = Just b
+    | otherwise = Nothing
+indexJ n (Append a l r) 
+    | findSize l > findSize r = indexJ (n - 1) l
+    | otherwise               = indexJ (n - 1) r
+    where findSize = getSize . size . tag
+
+dropJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
+dropJ 0 l = l
+dropJ _ Empty = Empty
+dropJ n (Single _ _) = Empty
+dropJ n (Append m l r) 
+
+takeJ :: (Sized b, Monoid b) => Int ->JoinList b a -> JoinList b a
+takeJ 0 l = Empty
+takeJ _ Empty = Empty
+takeJ n l@(Single _ _) = l
+takeJ n (Append a l r)
