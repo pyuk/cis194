522670cd3baf553b515879acf3d19acf37db86a5
pyuk <psi.razputin@gmail.com>
2016-08-11 00:50:33 -0500

    more week 7

--- week7/JoinList.hs
+++ week7/JoinList.hs
@@ -1,7 +1,10 @@
+{-# LANGUAGE FlexibleInstances, TypeSynonymInstances #-}
 module JoinList where
 
 import Data.Monoid
 import Sized
+import Scrabble
+import Buffer
 
 data JoinList m a = Empty
                   | Single m a
@@ -34,6 +37,9 @@ jlToList Empty = []
 jlToList (Single _ a) = [a]
 jlToList (Append _ l1 l2) = jlToList l1 ++ jlToList l2
 
+findSize :: (Sized b, Monoid b) => JoinList b a -> Int
+findSize = getSize . size . tag
+
 indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
 indexJ _ Empty = Nothing
 indexJ n _ | n < 0 = Nothing
@@ -41,18 +47,38 @@ indexJ n (Single a b)
     | n == 0    = Just b
     | otherwise = Nothing
 indexJ n (Append a l r) 
-    | findSize l > findSize r = indexJ (n - 1) l
-    | otherwise               = indexJ (n - 1) r
-    where findSize = getSize . size . tag
+    | n < findSize l = indexJ n l
+    | otherwise      = indexJ (n - findSize l) r
 
 dropJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
 dropJ 0 l = l
 dropJ _ Empty = Empty
 dropJ n (Single _ _) = Empty
 dropJ n (Append m l r) 
+    | n <= findSize l = dropJ n l +++ r
+    | otherwise       = dropJ (n - findSize l) r
 
 takeJ :: (Sized b, Monoid b) => Int ->JoinList b a -> JoinList b a
 takeJ 0 l = Empty
 takeJ _ Empty = Empty
 takeJ n l@(Single _ _) = l
 takeJ n (Append a l r)
+    | n <= findSize l = takeJ n l
+    | otherwise       = l +++ takeJ (n - findSize l) r
+
+--exercise 3
+
+scoreLine :: String -> JoinList Score String
+scoreLine xs = Single (scoreString xs) xs
+
+--exercise 4
+
+instance Buffer (JoinList (Score,Size) String) where
+    toString (Single m a) = a
+    toString (Append x r l) = toString r ++ toString r
+    fromString a = Single (scoreString a,1) a
+    line x j = indexJ x j
+    replaceLine x xs j = takeJ x j +++ fromString xs +++ dropJ (x + 1) j
+    numLines j = findSize j
+    value j = fromScore . f . tag $ j
+        where f (a,b) = a
